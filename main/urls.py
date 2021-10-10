8faa31a210bbcb18e54c182e24a5e01e53e5109f
ittyi#日本語ファイル名がエスケープされないように <vioiv.k4@gmail.com>
2021-10-11 05:40:55 +0900

    Login/Logout完成！

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='main'
 urlpatterns = [
+  path('', views.top_share, name='top_share'),
   path('chat/', views.chat, name='chat'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 # Create your views here.
+def top_share(request):
+    return render(request, 'main/top_share.html')
+
+
 def chat(request):
     return render(request, 'main/chat.html')
