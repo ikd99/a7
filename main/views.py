495447a62da35865368af639da11c5d584d1934d
hiyori99 <luluikdkoko@gmail.com>
2021-10-15 00:24:41 +0900

    チャットほぼ

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from .models import requests, user_info, favorite, messages
 from .form import PostAdd, TestForm
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -42,9 +45,11 @@ def getMyPage(request):
     }
     return render(request, 'main/mypage.html', my_dict)
 
+@login_required
 def chat(request, num):
     chat_room = requests.objects.get(id=num)
-    comment = messages.objects.all().filter(post_id=chat_room)
+    comment = messages.objects.all().filter(post_id=chat_room.id)
+    
     my_dict = {
         'form': TestForm,
         'comment': comment,
@@ -52,7 +57,16 @@ def chat(request, num):
     }
     print(chat_room.id)
     print(num)
+    print(request.user)
     if (request.method == "POST"):
-        # my_dict['form'] = TestForm(request.POST)
-        messages(user_id=User, post_id=chat_room.id, text=request.POST['text']).save   
+        my_dict['form'] = TestForm(request.POST)
+        user = request.user
+        # user = User.objects.get(id=user_id)
+        # messages.user_id = request.user
+        # messages.post_id = chat_room.id
+        # messages.text = request.POST['text']
+        # messages.save()
+        post_comment = messages(user_id=user, post_id = chat_room, text=request.POST['text'])
+        post_comment.save()
+        return HttpResponseRedirect('chat')
     return render(request, 'main/chat.html', my_dict)
