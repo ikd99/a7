49ee0bf012d39c73cc07e0138bf82c770ce41eec
hiyori99 <luluikdkoko@gmail.com>
2021-10-16 02:37:48 +0900

    投稿機能完成！

--- main/form.py
+++ main/form.py
@@ -15,5 +15,13 @@ class PostAdd(forms.ModelForm):
             'text':"詳細"
         }
 
+class PostForm(forms.Form):
+    title = forms.CharField(label='タイトル')
+    text = forms.CharField(label='詳細', widget=forms.Textarea)
+    departure_place = forms.CharField(label='出発地')
+    destination_place  = forms.CharField(label='目的地')
+    delivery_date = forms.DateField(label='配達希望日')
+    asking_price = forms.IntegerField(label='希望価格（日）')
+    
 class TestForm(forms.Form):
     text = forms.CharField(label='文字入力')
