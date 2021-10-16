d623c304f47fce783fd362973a61ef78088a6df9
Yuta Masuda <massu2000.1019@gmail.com>
2021-10-16 21:24:36 +0900

    🔥  不要なファイルの削除と文字の変更

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,3 +1,20 @@
+// index.htmlのjsここから
+
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
+
+// index.htmlのjsここまで
+
+
 const btn = document.getElementById("btn");
 const modal = document.getElementById("modal2");
 
