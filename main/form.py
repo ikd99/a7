e448a92678d6d18c120fff4d29358ce078dbb867
Yuta Masuda <massu2000.1019@gmail.com>
2021-10-16 16:24:39 +0900

    ♻️ リファクタリング

--- main/form.py
+++ main/form.py
@@ -2,19 +2,6 @@ from .models import requests
 from django import forms
 ...
 
-class PostAdd(forms.ModelForm):
-    class Meta:
-        model = requests
-        fields = ['title', 'share_or_not', 'text', 'departure_place', 'destination_place', 'delivery_date', 'asking_price']
-        labels={
-            'title':"タイトル",
-            'departure_place':"出発地",
-            'destination_place':"目的地",
-            'delivery_date':"配達希望日",
-            'asking_price':"希望価格(円)",
-            'text':"詳細"
-        }
-
 class PostForm(forms.Form):
     title = forms.CharField(label='タイトル')
     text = forms.CharField(label='詳細', widget=forms.Textarea)
